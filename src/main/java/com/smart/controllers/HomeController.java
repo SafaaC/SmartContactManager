0bd235267a94e623a1b87ef02e9b9d201f8bf49c
SafaaC <safaaacherak@gmail.com>
2023-06-26 14:24:23 +0530

    Setting up bootstrap and templates

--- src/main/java/com/smart/controllers/HomeController.java
+++ src/main/java/com/smart/controllers/HomeController.java
@@ -1,8 +1,21 @@
 package com.smart.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+	@GetMapping("/home")
+	public String home(Model model) {
+		model.addAttribute("title", "Home-Smart Contact Manager");
+		return "home";
+	}
+	
+	@GetMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title", "About-Smart Contact Manager");
+		return "about";
+	}
 }
