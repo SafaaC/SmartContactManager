a35dab520ae3afa227b5cf2eada9ba46fc4adaa0
SafaaC <safaaacherak@gmail.com>
2023-07-05 13:50:19 +0530

    User controller

---
+++ src/main/java/com/smart/controllers/UserController.java
@@ -0,0 +1,16 @@
+package com.smart.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@GetMapping("/index")
+	public String Dashboard() {
+		return "normal/user_dashboard";
+	}
+
+}
