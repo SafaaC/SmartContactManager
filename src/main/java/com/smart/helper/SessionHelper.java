59c99cafa27473b2c03e1ac7953e907ee6ff40be
SafaaC <safaaacherak@gmail.com>
2023-07-05 13:45:42 +0530

    remove session message

---
+++ src/main/java/com/smart/helper/SessionHelper.java
@@ -0,0 +1,21 @@
+package com.smart.helper;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Component
+public class SessionHelper {
+	public void removeMessageFromSession() {
+		try {
+			
+			HttpSession session = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest().getSession();
+			session.removeAttribute("message");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
