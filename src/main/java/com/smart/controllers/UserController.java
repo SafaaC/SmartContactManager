3b05556d6cc24db176376426ddd0b310ce26b868
SafaaC <safaaacherak@gmail.com>
2023-07-07 16:54:59 +0530

    made common data method using @ModelAttribute

--- src/main/java/com/smart/controllers/UserController.java
+++ src/main/java/com/smart/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smart.dao.UserRepository;
@@ -17,12 +18,18 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@GetMapping("/index")
-	public String Dashboard(Model model,Principal principal) {
+	@ModelAttribute
+	public void addCommonData(Model model,Principal principal) {
 		String userName=principal.getName();
 		User user=userRepository.getUserByUserName(userName);
 		model.addAttribute("user", user);
+		
+	}
+	@GetMapping("/index")
+	public String Dashboard(Model model,Principal principal) {
+		
 		return "normal/user_dashboard";
 	}
-
+	
+	
 }
