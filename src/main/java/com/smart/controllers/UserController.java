08144ef74875e2df68813ffc476861695ee500e9
SafaaC <safaaacherak@gmail.com>
2023-07-07 11:27:23 +0530

    passing (logined) user details to dashboard

--- src/main/java/com/smart/controllers/UserController.java
+++ src/main/java/com/smart/controllers/UserController.java
@@ -1,15 +1,27 @@
 package com.smart.controllers;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	@Autowired
+	private UserRepository userRepository;
 	
 	@GetMapping("/index")
-	public String Dashboard() {
+	public String Dashboard(Model model,Principal principal) {
+		String userName=principal.getName();
+		User user=userRepository.getUserByUserName(userName);
+		model.addAttribute("user", user);
 		return "normal/user_dashboard";
 	}
 
