486e452a1fc151d635ef5b77c6a1b25c98990aa1
SafaaC <safaaacherak@gmail.com>
2023-07-12 12:32:56 +0530

    minor changes

--- src/main/java/com/smart/dao/UserRepository.java
+++ src/main/java/com/smart/dao/UserRepository.java
@@ -1,19 +1,20 @@
 package com.smart.dao;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.smart.entities.User;
-
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import jakarta.transaction.Transactional;
-
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 	
 	
 	@Query("select u from User u where u.email=:email")
 	public User getUserByUserName(@Param("email") String email);
+	
+
 	
 }
