b413763259cf926d714d19feca8753ae12cb5ef4
SafaaC <safaaacherak@gmail.com>
2023-07-12 12:30:26 +0530

    view contacts handlers

--- src/main/java/com/smart/controllers/UserController.java
+++ src/main/java/com/smart/controllers/UserController.java
@@ -6,18 +6,24 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.smart.dao.ContactRepository;
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
@@ -31,6 +37,9 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private ContactRepository contactRepository;
+	
 	@ModelAttribute
 	public void addCommonData(Model model, Principal principal) {
 		String userName = principal.getName();
@@ -54,7 +63,7 @@ public class UserController {
 	}
 	
 	//processing add contact form
-	@PostMapping("process-contact")
+	@PostMapping("/process-contact")
 	public String processAddContactForm(@ModelAttribute Contact contact,
 										@RequestParam("profileImage") MultipartFile file,
 										Principal principal,
@@ -67,6 +76,8 @@ public class UserController {
 			if(file.isEmpty()) {
 				//if file is empty try our message
 				System.out.println("file is empty");
+				contact.setImage("contact.png");
+				
 			}else {
 				
 				//upload the file to folder and update the name in Contact
@@ -99,5 +110,30 @@ public class UserController {
 		
 		return "normal/add_contact_form";
 	}
+	
+	//show contacts handler
+	@GetMapping("/show-contacts/{page}")
+	public String showcontact(@PathVariable("page") Integer page,Model model,Principal principal) {
+		model.addAttribute("title", "View Contacts");
+		String userName = principal.getName();
+		User userByUserName = userRepository.getUserByUserName(userName);
+		int id = userByUserName.getId();
+		
+		Pageable pageable = PageRequest.of(page, 5);
+		
+		Page<Contact> contacts = contactRepository.findContactsByUser(id,pageable);
+		model.addAttribute("contacts", contacts);
+		model.addAttribute("currentPage", page);
+		model.addAttribute("totalPages", contacts.getTotalPages());
+		return "normal/show_contacts";
+	}
 
+	//show particular contact details
+	@GetMapping(value = "/contact/{cId}")
+	public String contact(Model model, @PathVariable("cId") int cId) {
+		Contact contact = contactRepository.getContactById(cId);
+		model.addAttribute("contact", contact);
+		model.addAttribute("title", "View Contact");
+		return "normal/contact";
+	}
 }
