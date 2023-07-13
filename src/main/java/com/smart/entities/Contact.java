6d1a34ee3e34534d162a2f76939c76b46f9142e3
SafaaC <safaaacherak@gmail.com>
2023-07-14 00:52:46 +0530

    json ignore

--- src/main/java/com/smart/entities/Contact.java
+++ src/main/java/com/smart/entities/Contact.java
@@ -1,5 +1,7 @@
 package com.smart.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -27,6 +29,7 @@ public class Contact {
 	private String description;
 	
 	@ManyToOne
+	@JsonIgnore
 	private User user;
 	
 	public User getUser() {
